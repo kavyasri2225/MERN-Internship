057f6ed6803b3db212056fd7b125f3dbf0e5793c
kavyasri2225 <sunkarikavyapatel@gmail.com>
2026-06-05 20:13:46 +0530

    Refactor App component by removing task handling update App .js
    Removed task management logic and UI elements.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/tasks")
-      .then((res) => setTasks(res.data));
-  }, []);
-
-  const addTask = () => {
-    if (!text) return;
-
-    axios
-      .post("http://localhost:5000/add", { text })
-      .then((res) => {
-        setTasks([...tasks, res.data]);
-        setText("");
-      });
-  };
-
   return (
     <div>
       <h1>MERN ToDo App</h1>
-
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-
-      <button onClick={addTask}>
-        Add Task
-      </button>
-
-      <ul>
-        {tasks.map((task) => (
-          <li key={task._id}>{task.text}</li>
-        ))}
-      </ul>
     </div>
   );
 }
